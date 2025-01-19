4c4185fb114f3d0b9ba7cca1952c57da4bb5c2ef
Muhammadzoir Mahmudov <kolen1470@gmail.com>
2025-01-19 21:41:06 +0500

    fix media mod

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
@@ -13,24 +19,29 @@ function Navbar() {
           </NavLink>
         </div>
 
+        {/* Кнопка для мобильного меню */}
+        <button className="menu-button" onClick={toggleMenu}>
+          ☰
+        </button>
+
         {/* Навигационные ссылки */}
-        <div className="navbar-links">
-          <NavLink to="/" className="nav-item">
+        <div className={`navbar-links ${menuOpen ? 'open' : ''}`}>
+          <NavLink to="/" className="nav-item" onClick={() => setMenuOpen(false)}>
             Home
           </NavLink>
-          <NavLink to="/about" className="nav-item">
+          <NavLink to="/about" className="nav-item" onClick={() => setMenuOpen(false)}>
             About
           </NavLink>
-          <NavLink to="/skills" className="nav-item">
+          <NavLink to="/skills" className="nav-item" onClick={() => setMenuOpen(false)}>
             Skills
           </NavLink>
-          <NavLink to="/projects" className="nav-item">
+          <NavLink to="/projects" className="nav-item" onClick={() => setMenuOpen(false)}>
             Projects
           </NavLink>
-          <NavLink to="/education" className="nav-item">
+          <NavLink to="/education" className="nav-item" onClick={() => setMenuOpen(false)}>
             Education
           </NavLink>
-          <NavLink to="/resume" className="nav-item">
+          <NavLink to="/resume" className="nav-item" onClick={() => setMenuOpen(false)}>
             Resume
           </NavLink>
         </div>
